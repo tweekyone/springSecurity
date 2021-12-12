665e23ec4ff15d8da9dd31a0af27ff10c71a508c
Evgenii Ponukhov <evgenii_ponukhov@epam.com>
2021-12-12 19:10:55 +0300

    added JwtUtil

--- src/main/java/ru/tweekyone/security/configuration/WebSecurityConfig.java
+++ src/main/java/ru/tweekyone/security/configuration/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.tweekyone.security.service.UserService;
@@ -36,6 +37,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //disable CSRF, enable CORS
         http.cors().disable().csrf().disable();
+
+        //Stateless because user will be authorised by token
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests()
                 .antMatchers("/hello").hasRole("ADMIN")
